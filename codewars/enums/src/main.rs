9dc381e2607381a15fe389dcb3dcdd0b0b87c118
ben <bm@wps.de>
2019-05-28 20:12:49 +0200

    first examples enums, no output

--- codewars/enums/src/main.rs
+++ codewars/enums/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+enum IpAddrKind {
+    V4,
+    V6,
 }
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+// More concise way 
+enum IpAddress {
+    V4(String),
+    V6(String),
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+
+    let improved_home = IpAddress::V4(String::from("127.0.0.1"));
+    let improved_loopback = IpAddress::V6(String::from("::1"));
+}
+
+
+fn route(ip_kind: IpAddrKind) {}
